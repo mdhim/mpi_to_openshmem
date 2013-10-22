ce6caa3aa6e15ac2e47838d9e831bb22d6220b1a
gingery <gingery@lanl.gov>
2013-10-22 11:34:04 -0600

    Update mpi_to_openshmem.h
    Sync with current work.

--- src/mpi_to_openshmem.h
+++ src/mpi_to_openshmem.h
@@ -11,16 +11,71 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <mpi.h>
+//#include <mpi.h>
 #include <shmem.h>
 #include <stdint.h>
 #include <pthread.h>
 #include "Mlog/mlog.h"
 #include "Mlog/mlogfacs.h"
 
-/* Success/fail return values */
-#define MPI_TO_OPENSHMEM_SUCCESS	1
-#define MPI_TO_OPENSHMEM_FAIL		0
+long pSync[_SHMEM_BCAST_SYNC_SIZE];
+
+/**** from mpi.h.in									****/
+#define MPI_COMM_WORLD		91
+
+#define MPI_SUCCESS          0      /* Successful return code */
+#define MPI_ERR_NO_MEM      34      /* Alloc_mem could not allocate memory */
+
+typedef int MPI_Datatype;
+#define MPI_CHAR           ((MPI_Datatype)1)
+#define MPI_UNSIGNED_CHAR  ((MPI_Datatype)2)
+#define MPI_BYTE           ((MPI_Datatype)3)
+#define MPI_SHORT          ((MPI_Datatype)4)
+#define MPI_UNSIGNED_SHORT ((MPI_Datatype)5)
+#define MPI_INT            ((MPI_Datatype)6)
+#define MPI_UNSIGNED       ((MPI_Datatype)7)
+#define MPI_LONG           ((MPI_Datatype)8)
+#define MPI_UNSIGNED_LONG  ((MPI_Datatype)9)
+#define MPI_FLOAT          ((MPI_Datatype)10)
+#define MPI_DOUBLE         ((MPI_Datatype)11)
+#define MPI_LONG_DOUBLE    ((MPI_Datatype)12)
+#define MPI_LONG_LONG_INT  ((MPI_Datatype)13)
+/* MPI_LONG_LONG is in the complete ref 2nd edition, though not in the 
+ standard.  Rather, MPI_LONG_LONG_INT is on page 40 in the HPCA version */
+#define MPI_LONG_LONG      ((MPI_Datatype)13)
+
+/* For supported thread levels */
+#define MPI_THREAD_SINGLE		0
+#define MPI_THREAD_FUNNELED		1
+#define MPI_THREAD_SERIALIZED	2
+#define MPI_THREAD_MULTIPLE		3
+/****                									****/
+
+
+/**** from mpidefs.h									****/
+/* 
+ Status object.  It is the only user-visible MPI data-structure 
+ The "count" field is PRIVATE; use MPI_Get_count to access it. 
+ */
+int MPI_SOURCE;
+int MPI_TAG;
+int MPI_ERROR;
+
+/* MPI_STATUS_SIZE is not strictly required in C; however, it should match
+the value for Fortran */
+
+#define MPI_STATUS_SIZE 4
+typedef struct { 
+    int count;
+    int MPI_SOURCE;
+    int MPI_TAG;
+    int MPI_ERROR;
+#if (MPI_STATUS_SIZE > 4)
+    int extra[MPI_STATUS_SIZE - 4];
+#endif
+} MPI_Status;
+/****                									****/
+
 
 // The following structures may be more than we actually need.
 // Pruning and growing will occur as time progresses.
@@ -39,16 +94,19 @@ typedef struct MPID_Group {
 } MPID_Group;
 
 typedef struct MPID_Comm {
-    int				remote_size;  /* Value of MPI_Comm_(remote)_size */
+    //int				remote_size;  /* Value of MPI_Comm_(remote)_size */
     int				rank;         /* Value of MPI_Comm_rank */
     int				local_size;   /* Value of MPI_Comm_size for local group */
     MPID_Group		*local_group;     /* Groups in communicator. */
-	struct MPID_Comm	*node_comm;       /* Comm of processes in this comm that are on the same node as this process. */
-	struct MPID_Comm	*node_roots_comm; /* Comm of root processes for other nodes. */
-	struct MPID_Comm	*comm_next;       /* Provides a chain through all active communicators */
+	//struct MPID_Comm	*node_comm;       /* Comm of processes in this comm that are on the same node as this process. */
+	//struct MPID_Comm	*node_roots_comm; /* Comm of root processes for other nodes. */
+	//struct MPID_Comm	*comm_next;       /* Provides a chain through all active communicators */
+	void			*symmetricHeapPtr;
 } MPID_Comm;
 
-
+typedef int        MPI_Comm;
+typedef MPID_Group MPI_Group;
+typedef int        MPI_Request;
 
 /* Define all of the subroutines */
 //struct mpi_to_openshmem_t * ( int mpiType, int required, int *provided, int debugLevel ); 
@@ -56,7 +114,9 @@ int MPI_Init( int *argc, char ***argv );
 int MPI_Init_thread( int *argc, char ***argv, int required, int *provided );
 int MPI_Abort (MPI_Comm comm, int errorcode);
 int MPI_Barrier(MPI_Comm comm);
+
 int MPI_Bcast(void *buffer, int count, MPI_Datatype datatype, int root, MPI_Comm comm);
+
 int MPI_Comm_create(MPI_Comm comm, MPI_Group group, MPI_Comm *newcomm);
 int MPI_Comm_dup(MPI_Comm comm, MPI_Comm *newcomm);
 int MPI_Comm_group(MPI_Comm comm,	MPI_Group *group);
