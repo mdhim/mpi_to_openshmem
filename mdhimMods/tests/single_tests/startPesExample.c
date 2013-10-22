d8f06c20d7402d50411a88cc3dd7a7e940a7f5d3
gingery <gingery@lanl.gov>
2013-10-22 11:42:11 -0600

    Create startPesExample.c
    Syncing with my local files...

---
+++ mdhimMods/tests/single_tests/startPesExample.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <shmem.h>
+#include "mpi_to_openshmem.h"
+
+
+long pSync[_SHMEM_BCAST_SYNC_SIZE];
+
+void initOpenShmem(){
+	printf ("initOpenShmem.\n");
+		
+	start_pes (0);
+
+	int rank = _my_pe ();
+	int npes = _num_pes ();
+	
+	printf ("initOpenShmem. rank = %d, npes = %d\n", rank, npes);	
+	
+}
+
+int main(int argc, char **argv) {
+	printf ("startPESExample: begin  *************\n");
+
+	initOpenShmem();
+	printf ("After calling initOpenShmem  *************\n");
+	
+	start_pes(0);
+
+       	return 0;
+}
