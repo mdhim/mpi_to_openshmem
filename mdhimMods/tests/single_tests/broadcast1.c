723ffce2efa52b07af2f2254b7518a9872c6f12a
gingery <gingery@lanl.gov>
2013-10-23 09:34:51 -0600

    Update broadcast1.c
    Updated version

--- mdhimMods/tests/single_tests/broadcast1.c
+++ mdhimMods/tests/single_tests/broadcast1.c
@@ -1,16 +1,15 @@
-*                                                                                                                             
-*  broadcast1.c                                                                                                               
-*  mpiToOpenshmem                                                                                                             
-*                                                                                                                             
-*  Created by gingery on 10/16/13.                                                                                            
-*  Copyright 2013 LANL. All rights reserved.                                                                                  
-*                                                                                                                             
+*  broadcast1.c
+*  mpiToOpenshmem
+*
+*  Created by gingery on 10/16/13.
+*  Copyright 2013 LANL. All rights reserved.
+*
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <shmem.h>
-//#include <mpi.h>                                                                                                             
+//#include <mpi.h>
 #include "mpi_to_openshmem.h"
 
 
@@ -38,23 +37,27 @@ int main(int argc, char **argv) {
 	MPI_Comm_size ( comm, &npes);
 	
 	int my_pe = shmem_my_pe();
-	printf("Main,  my_pe: %-8d, rank: %d, size: %d\n", my_pe, rank, npes);
+	//printf("Main,  my_pe: %-8d, rank: %d, size: %d\n", my_pe, rank, npes);
 	
-	bufsize = npes * sizeof( *source ) * 3;
-	size = npes * 3;
+	size = npes * 3; 
+	bufsize = size * sizeof( *source );
+	
 	
 	source = (long *) shmalloc (bufsize);
 	
 	for (i = 0; i < size; i += 1)
     {
 		source[i] = i + 1;
-		printf("Main. my_pe: %-8d, source[%d] = %d\n", my_pe, i, source[i]);
+		//printf("Main. my_pe: %-8d, source[%d] = %d\n", my_pe, i, source[i]); 
 	}
 	
-	printf("Before Calling mpi_bcast.\n");
+	//printf("Before Calling mpi_bcast.\n");
 	MPI_Bcast(source, size, MPI_LONG, 0, comm);
-	printf("After Calling mpi_bcast.\n");
+	//printf("After Calling mpi_bcast.\n");
+	
+	printf("Before Calling mpi_bcast. Root = 1\n");
+	MPI_Bcast(source, size, MPI_LONG, 1, comm);
+	//printf("After Calling mpi_bcast.\n");
 	
 	MPI_Finalize();
 }
-
