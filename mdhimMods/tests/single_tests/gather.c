c3afb928b9585b0f60fe3baedd3e29da928a2d28
gingery <gingery@lanl.gov>
2013-11-05 10:40:50 -0700

    Update gather.c
    Updated to the latest test code.

--- mdhimMods/tests/single_tests/gather.c
+++ mdhimMods/tests/single_tests/gather.c
@@ -1,10 +1,11 @@
-*  gather.c
-*  mpiToOpenshmem
-*
-*  Created by gingery on 10/31/13.
-*  Copyright 2013 LANL. All rights reserved.
-*
-*/
+/*                                                                                                                             
+ *  gather.c                                                                                                                   
+ *  mpiToOpenshmem                                                                                                             
+ *                                                                                                                             
+ *  Created by gingery on 10/31/13.                                                                                            
+ *  Copyright 2013 LANL. All rights reserved.                                                                                  
+ *                                                                                                                             
+ */
 
 #include <stdio.h>
 #include <string.h>
@@ -12,11 +13,12 @@
 #include <shmem.h>
 #include "mpi_to_openshmem.h"
 
-#define DST_SIZE 20
+#define DST_SIZE 40
 
-static long src[4] = { 11, 12, 13, 14 };
-static long dst[DST_SIZE];
+//static long pSync[_SHMEM_BCAST_SYNC_SIZE];                                                                                   
 
+static int src[11] = { 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21 };
+static int dst[DST_SIZE];
 
 int me, npes;
 
@@ -33,28 +35,36 @@ int main(int argc, char *argv[]){
 	MPI_Comm_size(MPI_COMM_WORLD, &npes);
 	MPI_Comm_rank(MPI_COMM_WORLD, &me);
 	
-	for (i = 0; i < DST_SIZE; i++)
-    {
+	for (i = 0; i < DST_SIZE; i++){
 		dst[i] = -1;
-    }
-	
-	if (me == 1){
-		src[1]=21;
 	}
-		
-	MPI_Gather( src, 4, MPI_INT, dst, 20, MPI_INT, 0, MPI_COMM_WORLD); 
-	show_dst ("AFTER");
+	
+	int count = 10;
+	
+	// set the data, so I can see it better                                                                                      
+	for (i = 0; i < count; i++){
+		src[i] = src[i] + me;
+	}
+	printf ("Debug: src[%d/%d] =", me, npes);
+	for (i = 0; i < count; i += 1) {
+		printf (" %d", src[i]);
+	}
+	printf("\n");
+	
+	
+	// try to gather all data                                                                                                    
+	int root = 2;
+	//shmem_barrier_all();                                                                                                       
+	MPI_Gather (src, count, MPI_INT, dst, count, MPI_INT, root, MPI_COMM_WORLD);
+	// shmem_int_put(&dst[count*me], src, count, root);                                                                          
+	
+	if (me == root){
+		printf ("After gather,: dst[%d/%d] =", me, npes);
+		for (i = 0; i < DST_SIZE; i += 1) {
+			printf (" %d", dst[i]);
+		}
+		printf ("\n");
+	}
 	
 	return 0;
-}
-
-show_dst (char *tag)
-{
-	int i;
-	printf ("%8s: dst[%d/%d] =", tag, me, npes);
-	for (i = 0; i < DST_SIZE; i += 1)
-    {
-		printf (" %ld", dst[i]);
-    }
-	printf ("\n");
 }
