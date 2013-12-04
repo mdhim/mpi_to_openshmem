6a282942c66dfa1c9d3aed02e3e726ba58e61ac0
gingery <gingery@lanl.gov>
2013-12-04 11:11:30 -0700

    Update ds_leveldb.h
    Sync'd with mdhim-tng

--- mdhimMods/src/ds_leveldb.h
+++ mdhimMods/src/ds_leveldb.h
@@ -8,13 +8,15 @@ int mdhim_leveldb_put(void *dbh, void *key, int key_len, void *data, int32_t dat
 int mdhim_leveldb_get(void *dbh, void *key, int key_len, void **data, int32_t *data_len, 
 		      struct mdhim_store_opts_t *mstore_opts);
 int mdhim_leveldb_get_next(void *dbh, void **key, int *key_len, 
-			   void **data, int32_t *data_len, void **iterator,
+			   void **data, int32_t *data_len,
 			   struct mdhim_store_opts_t *mstore_opts);
 int mdhim_leveldb_get_prev(void *dbh, void **key, int *key_len, 
-			   void **data, int32_t *data_len, void **iterator,
+			   void **data, int32_t *data_len,
 			   struct mdhim_store_opts_t *mstore_opts);
 int mdhim_leveldb_close(void *dbh, void *dbs, struct mdhim_store_opts_t *mstore_opts);
 int mdhim_leveldb_del(void *dbh, void *key, int key_len, 
 		      struct mdhim_store_opts_t *mstore_opts);
-int mdhim_leveldb_iter_free(void **iterator);
 int mdhim_leveldb_commit(void *dbh);
+int mdhim_leveldb_batch_put(void *dbh, void **key, int32_t *key_lens, 
+			    void **data, int32_t *data_lens, int num_records,
+			    struct mdhim_store_opts_t *mstore_opts);
