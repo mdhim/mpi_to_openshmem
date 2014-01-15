f26ff9ff2fc76d9b33c5dd43d01d77758f0dd0df
gingery <gingery@lanl.gov>
2014-01-15 14:59:55 -0700

    Update broadcast1.c
    Made it a pass/fail test.  Removed the shmalloc, since MPI_Bcast no longer needs it!

--- mdhimMods/tests/single_tests/broadcast1.c
+++ mdhimMods/tests/single_tests/broadcast1.c
@@ -1,31 +1,25 @@
-*  broadcast1.c
-*  mpiToOpenshmem
-*
-*  Created by gingery on 10/16/13.
-*  Copyright 2013 LANL. All rights reserved.
-*
-*/
+/*
+ *  broadcast1.c
+ *  mpiToOpenshmem
+ *
+ *  Created by gingery on 10/16/13.
+ *  Copyright 2013 LANL. All rights reserved.
+ *
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <shmem.h>
-//#include <mpi.h>
 #include "mpi_to_openshmem.h"
 
 
 int main(int argc, char **argv) {
-	printf ("broadcast1: begin  *************\n");
-	
 	int i, ret;
 	long *source;
 	int  bufsize;
 	int  rank, npes;
 	int  size;
 	int  provided;
-	MPI_Comm comm;
-	
-	printf ("Before calling mpi_init  *************\n");
-	
+	int  isGood = TRUE;
 	
 	ret = MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &provided);
 	if (ret != MPI_SUCCESS) {
@@ -33,31 +27,36 @@ int main(int argc, char **argv) {
 		exit(1);
 	}
 	
-	MPI_Comm_rank ( comm, &rank);
-	MPI_Comm_size ( comm, &npes);
-	
+	MPI_Comm_rank ( MPI_COMM_WORLD, &rank);
+	MPI_Comm_size ( MPI_COMM_WORLD, &npes);
+    
 	int my_pe = shmem_my_pe();
 	//printf("Main,  my_pe: %-8d, rank: %d, size: %d\n", my_pe, rank, npes);
 	
 	size = npes * 3; 
 	bufsize = size * sizeof( *source );
+	source = (long *)malloc (bufsize);
 	
-	
-	source = (long *) shmalloc (bufsize);
-	
-	for (i = 0; i < size; i += 1)
-    {
-		source[i] = i + 1;
-		//printf("Main. my_pe: %-8d, source[%d] = %d\n", my_pe, i, source[i]); 
+	for (i = 0; i < size; i++){
+		if (rank == 0) source[i] = 1 + i;
+		else  source[i] = 999;
 	}
 	
-	//printf("Before Calling mpi_bcast.\n");
-	MPI_Bcast(source, size, MPI_LONG, 0, comm);
-	//printf("After Calling mpi_bcast.\n");
+	MPI_Bcast(source, size, MPI_LONG, 0, MPI_COMM_WORLD);
+	//int root = 0;
+	//shmem_broadcast64( target, source, size, root, 0, 0, npes, pSync);
 	
-	printf("Before Calling mpi_bcast. Root = 1\n");
-	MPI_Bcast(source, size, MPI_LONG, 1, comm);
-	//printf("After Calling mpi_bcast.\n");
+	// Check that the value is as expected
+	for (i = 0; i < size; i++){
+		if ( source[i] != (i+1) ){
+			isGood = FALSE;
+			printf("Failed. my_pe: %-8d, source[%d] = %ld, expected %d\n", my_pe, i, source[i], (i+1));
+		}
+	}
+	if (isGood){
+		printf("Passed, my_pe: %d\n",my_pe);
+	}
 	
-	MPI_Finalize();
+	return isGood;
+	
 }
