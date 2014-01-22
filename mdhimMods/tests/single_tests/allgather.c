7f78f5e34fd1bc946adbbd57041da437aa3c11f1
Ginger Young <gingery@lanl.gov>
2014-01-22 14:52:23 -0700

    Making allgather break - this is another instance of need ing to have symmetric memory for it to work...

--- mdhimMods/tests/single_tests/allgather.c
+++ mdhimMods/tests/single_tests/allgather.c
@@ -13,63 +13,69 @@
 #include <shmem.h>
 #include "mpi_to_openshmem.h"
 
-#define DST_SIZE 20
+#define DEST_SIZE 20
+#define SRC_SIZE   4
 
 //static long pSync[_SHMEM_BCAST_SYNC_SIZE];
 
-static long src[4] = { 11, 12, 13, 14 };
-static long dst[DST_SIZE];
+//static long src[4] = { 11, 12, 13, 14 };
+//static long dst[DST_SIZE];
 
 
 int me, npes;
 
 int main(int argc, char *argv[]){
-  int i, ret;
-  int provided;
-
-  ret = MPI_Init_thread(&argc, &argv,  MPI_THREAD_MULTIPLE, &provided);
-  if (ret != MPI_SUCCESS) {
-    printf("Error initializing MPI with threads\n");
-    exit(1);
-  }
-  
-  MPI_Comm_size(MPI_COMM_WORLD, &npes);
-  MPI_Comm_rank(MPI_COMM_WORLD, &me);
-
-  for (i = 0; i < DST_SIZE; i++)
-    {
-      dst[i] = -1;
-    }
-
-  if (me == 1){
-    src[1]=21;
-  }
-
-  /*
-  for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1)
-    {
-      pSync[i] = _SHMEM_SYNC_VALUE;
-    }
-  
-  shmem_barrier_all ();
-  
-  shmem_fcollect64 (dst, src, me+1, 0, 0, npes, pSync);
-  */
-
-  MPI_Allgather( src, 4, MPI_INT, dst, 20, MPI_INT, MPI_COMM_WORLD); 
-  show_dst ("AFTER");
-  
-  return 0;
+	int i, ret;
+	int provided;
+	long *src;
+	long *dest;
+	
+	ret = MPI_Init_thread(&argc, &argv,  MPI_THREAD_MULTIPLE, &provided);
+	if (ret != MPI_SUCCESS) {
+		printf("Error initializing MPI with threads\n");
+		exit(1);
+	}
+	
+	src = malloc ( SRC_SIZE  * sizeof(long) );
+	dest = malloc ( DEST_SIZE * sizeof(long) );  
+	
+	MPI_Comm_size(MPI_COMM_WORLD, &npes);
+	MPI_Comm_rank(MPI_COMM_WORLD, &me);
+	
+	for (i = 0; i <SRC_SIZE; i++){
+		src[i] = 11 + i;
+	}
+	for (i = 0; i < DEST_SIZE; i++) {
+		dest[i] = -1;
+	}
+	
+	if (me == 1){
+		src[1]=21;
+	}
+	
+	/*
+	 for (i = 0; i < _SHMEM_BCAST_SYNC_SIZE; i += 1)
+	 {
+	 pSync[i] = _SHMEM_SYNC_VALUE;
+	 }
+	 
+	 shmem_barrier_all ();
+	 
+	 shmem_fcollect64 (dst, src, me+1, 0, 0, npes, pSync);
+	 */
+	
+	MPI_Allgather( src, SRC_SIZE, MPI_INT, dest, DEST_SIZE, MPI_INT, MPI_COMM_WORLD); 
+	show_dst ("AFTER", dest);
+	
+	return 0;
 }
 
-show_dst (char *tag)
-{
-  int i;
-  printf ("%8s: dst[%d/%d] =", tag, me, npes);
-  for (i = 0; i < DST_SIZE; i += 1)
-    {
-      printf (" %ld", dst[i]);
+void show_dst (char *tag, long *dest){
+	int i;
+	printf ("%8s: dest[%d/%d] =", tag, me, npes);
+	for (i = 0; i < DEST_SIZE; i += 1){
+		printf (" %ld", dest[i]);
     }
-  printf ("\n");
+	printf ("\n");
 }
 
